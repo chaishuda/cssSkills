3a32fdfcf1ffc79cab0129a093fc9953141ebfa8
柴树达 <W__apple@163.com>
2025-05-20 18:21:21 +0800

    refactor: 移除 HelloWorld 组件并调整 App 组件布局
    - 删除了 HelloWorld 组件文件
    - 在 App.vue 中移除了 HelloWorld 组件的引用和使用
    - 调整了 App.vue 的模板布局，使内容居中显示
    - 在 vite.config.ts 中添加了服务器端口配置

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
   <div>
@@ -11,9 +9,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
+<script setup lang="ts">
+</script>
 <style scoped>
 .logo {
   height: 6em;
